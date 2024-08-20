6ff7c7d2be31b7927ecaa12737e73e99b2ada995
Gabriel Larsão Lugli <146869089+gllugli@users.noreply.github.com>
2024-08-20 13:01:09 -0300

    Create Aula01 - Ex04.java

---
+++ Aula01 - Ex04.java
@@ -0,0 +1,42 @@
+package a01;
+
+import java.util.Scanner;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double A;
+		double B;
+		double C;
+		
+		double areaTriangulo;
+		double areaCirculo;
+		double areaTrapezio;
+		double areaQuadrado;
+		double areaRetangulo;
+		
+		System.out.println("Digite o valor de A: ");
+		sc.nextDouble();
+		
+		System.out.println("Digite o valor de B: ");
+		sc.nextDouble();
+		
+		System.out.println("Digite o valor de C: ");
+		sc.nextDouble();
+		
+		areaTriangulo = ;
+		
+		areaCirculo = 2 * 3.14159 * C;
+		
+		areaTrapezio = ;
+		
+		areaQuadrado = Math.pow(B, 2);
+		
+		areaRetangulo = A * B;
+		
+		sc.close();
+	}
+
+}
